e8bfa614718de43a293669cdb5fe306bea72eb5d
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2024-11-20 15:45:55 +0700

    Update Homework1_ss2.cpp

--- Homework1_ss2.cpp
+++ Homework1_ss2.cpp
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
-	int num1 = 10;
-	float mark = 7.9;
-	char alphabet = 'A';
+	int num1 = 10; //Kieu du lieu so nguyen
+	float mark = 7.9; //Kieu du lieu so thuc
+	char alphabet = 'A'; //Kieu du lieu ky tu
 	printf("number1 = %d",num1);
 	printf("\nDiem tong ket cuoi ki I =%f ",mark);
 	printf("\nChu cai alphabet dau tien la: %c ",alphabet);
